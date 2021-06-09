0f85a547c86fa70040793a558956922e3325fb72
Marco Mangan <marco.mangan@gmail.com>
2021-06-09 10:13:11 -0300

    Compartilhamento inicial do projeto

---
+++ Conjunto.java
@@ -0,0 +1,18 @@
+
+public class Conjunto
+{
+    /**
+     * Criar testes no BlueJ!
+     * 
+     * [] [] -> []
+     * [] [1,2,3] -> [1, 2, 3]
+     * [1, 2] [9] -> [1, 2, 9]
+     * [1] [1] -> [1, 1]
+     */
+    public static int[] unir(int[] a, int[] b)
+    {
+        int[] r = {};
+        // Completar!
+        return r;
+    }
+}
---
+++ ConjuntoTest.java
@@ -0,0 +1,62 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * A classe de teste ConjuntoTest.
+ *
+ * @author  (seu nome)
+ * @version (um número da versão ou uma data)
+ */
+public class ConjuntoTest
+{
+    /**
+     * Construtor default para a classe de teste ConjuntoTest
+     */
+    public ConjuntoTest()
+    {
+    }
+
+    /**
+     * Define a 'fixture' do teste.
+     *
+     * Chamado antes de cada método de caso de teste.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Desfaz a 'fixture' do teste.
+     *
+     * Chamado após cada método de teste de caso.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void VazioEVazioResultaEmVazio()
+    {
+        int[] a = {};
+        int[] b = {};
+        int[] r = {};
+        assertArrayEquals(r, Conjunto.unir(a, b));
+    }
+
+    @Test
+    public void VazioE123ResultaEm123()
+    {
+        int[] a = {};
+        int[] b = {1, 2, 3};
+        int[] r = {1, 2, 3};
+        assertArrayEquals(r, Conjunto.unir(a, b));
+    }
+
+}
+
